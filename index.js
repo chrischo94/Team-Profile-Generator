9628ca995960ffb5bfd0979733e24c11349acaf8
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-03 13:11:35 +0900

    syntax errors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
-const Intern = require('./lib/Intern');
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const myTeam = []
